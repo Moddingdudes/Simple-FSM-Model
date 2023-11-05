e8c2d37d45cdf6c0bbc3d598aa9aad741d5a9407
Moddingdudes <halestorm512@gmail.com>
2023-11-05 01:39:16 -0500

    Add flip decision to transitions

--- FSMTransition.cs
+++ FSMTransition.cs
@@ -12,6 +12,7 @@ namespace CyberneticStudios.FSM
         [SerializeField] private FSMDecision _decision;
         [SerializeField] private FSMState _fromState;
         [SerializeField] private FSMState _toState;
+        [SerializeField] private bool _flipDecision; //Flips the decision value when the decision state is changed. This is useful for configuring bidirectional transitions without writing 2 FSMDecision's
 
         //Passes in the before state and after state
         public event System.Action<FSMState, FSMState> OnTransitionStateChanged;
@@ -28,6 +29,9 @@ namespace CyberneticStudios.FSM
 
         private void OnDecisionStateChanged(bool decisionState)
         {
+            //Apply decision flip if necessary
+            decisionState = _flipDecision ? !decisionState : decisionState;
+
             if (decisionState)
                 OnTransitionStateChanged?.Invoke(_fromState, _toState);
         }
