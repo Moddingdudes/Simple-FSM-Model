62463097f7786bead29a5b9e60b879eae47d8e21
Moddingdudes <halestorm512@gmail.com>
2023-11-05 13:32:18 -0500

    Add FSMAction activate/disable virtual overrides

--- FSMAction.cs
+++ FSMAction.cs
@@ -23,16 +23,23 @@ namespace CyberneticStudios.FSM
         protected virtual void Start() { }
         protected virtual void OnDestroy() { }
 
+        protected virtual void OnActionActivated() { }
+        protected virtual void OnActionDisabled() { }
+
         public virtual void ActionUpdate() { }
 
         public void ActivateAction()
         {
             _actionActivated = true;
+
+            OnActionActivated();
         }
 
         public void DisableAction()
         {
             _actionActivated = false;
+
+            OnActionDisabled();
         }
     }
 }
