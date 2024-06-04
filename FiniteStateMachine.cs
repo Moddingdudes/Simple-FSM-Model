ac4a82ab934ecbf307079ebf7fa1f0686b111887
Moddingdudes <halestorm512@gmail.com>
2024-06-04 15:11:42 -0400

    feat: allow enable/disable of FSM

--- FiniteStateMachine.cs
+++ FiniteStateMachine.cs
@@ -12,6 +12,7 @@ namespace CyberneticStudios.FSM
         [SerializeField] private List<FSMTransition> _transitions = new List<FSMTransition>();
         [SerializeField] private FSMState _defaultState;
         [SerializeField] private bool _debugTransitionStateChanges;
+        [SerializeField] private bool _fsmEnabled = true;
 
         private FSMState _activeState;
 
@@ -19,16 +20,46 @@ namespace CyberneticStudios.FSM
             
         private void Start()
         {
-            ChangeState(null, _defaultState);
+            _activeState = _defaultState;
 
             foreach (FSMTransition transition in _transitions)
             {
                 transition.OnTransitionStateChanged += OnTransitionStateChanged;
             }
+
+            if (_fsmEnabled)
+                ChangeState(null, _defaultState);
+        }
+
+        public void SetFSMState(FSMState state)
+        {
+            if (!_fsmEnabled)
+            {
+                Debug.LogError("Cannot change FSM state while FSM is disabled.");
+                return;
+            }
+
+            ChangeState(_activeState, state);
+        }
+
+        public void EnableFSM()
+        {
+            _fsmEnabled = true;
+
+            _activeState?.ActivateState();
+        }
+
+        public void DisableFSM()
+        {
+            _fsmEnabled = false;
+
+            _activeState?.DisableState();
         }
 
         private void OnTransitionStateChanged(FSMState fromState, FSMState toState)
         {
+            if (!_fsmEnabled) return;
+
             ChangeState(fromState, toState);
 
             if (_debugTransitionStateChanges)
@@ -37,6 +68,12 @@ namespace CyberneticStudios.FSM
 
         private void ChangeState(FSMState oldState, FSMState newState)
         {
+            if (!_fsmEnabled)
+            {
+                Debug.LogError("Cannot change state while FSM is disabled.");
+                return;
+            }
+
             _activeState = newState;
 
             oldState?.DisableState();
